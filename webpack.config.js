4ee715798b040a0657592c2eadefd7ca4328c99d
Cindy Dorantes <cindym.dorantes@gmail.com>
2022-07-28 11:41:15 -0500

    Setup local dev server

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,23 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
-        template: './src/index.html',
+      template: './src/index.html',
     }),
   ],
   output: {
-    filename: 'main.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
-  mode: 'development',
+  optimization: {
+    runtimeChunk: 'single',
+  },
   module: {
     rules: [
       {
